4e81cec458641f21649d55c970b56a2427d6cc55
Kiara <kiaraymg@gmail.com>
2020-09-09 15:12:01 +0200

    Finished the price filtering and started displaying information on the front.

--- routes/api/ads.js
+++ routes/api/ads.js
@@ -6,12 +6,12 @@ const Ads = require('../../models/Ads');
 /* To save the images on local storage */
 // const upload = multer({ dest: 'uploads/'});
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'uploads/');
+    destination: function (req, file, callback) { // The place where uploads should be stored
+        callback(null, 'uploads/');
     },
-    filename: function (req, file, cb) {
-        const myFilename = `ads_${file.fieldname}_${Date.now()}_${file.originalname}`;
-        cb(null, myFilename);
+    filename: function (req, file, cb) { // The new filename based on the file's name and date now.
+        const newFilename = `ads_${file.fieldname}_${Date.now()}`;
+        callback(null, newFilename);
     }
 });
 const upload = multer({ storage: storage });
@@ -21,12 +21,10 @@ router.get('/', async function (req, res, next) {
     try {
         // http://localhost:3000/api/ads?name=Samsung
         const name = req.query.name;
-        // http://localhost:3000/api/ads?priceStart=360
-        const priceMin = req.query.price;
-        // http://localhost:3000/api/ads?priceMax=360
-        const priceMax = req.query.price;
+        // http://localhost:3000/api/ads?price=360
+        const price = req.query.price;
         // http://localhost:3000/api/ads?sale=true
-        const sale = req.query.sale
+        const sale = req.query.sale;
 
         // http://localhost:3000/api/ads?limit=8
         const limit = parseInt(req.query.limit || 10);
@@ -46,12 +44,25 @@ router.get('/', async function (req, res, next) {
             filter.name = new RegExp('^' + name, 'i');
         }
 
-        if (priceMin) {
-            filter.price = { $gte: priceMin }
-        }
-
-        if (priceMax) {
-            filter.price = { $lte: priceMax }
+        if (price) {
+            const priceGroups = price.match(/(\d+)/gm);
+            console.log("price --->" + price)
+            console.log("priceGroups --->" + priceGroups[0], priceGroups[1]);
+            if ((/-/gm).test(price)) { // If we detect a separator on the price, it will be filtered using the lte and/or gte
+                if ((/\d+-/gm).test(price)) {
+                    let minPrice = priceGroups[0];
+                    filter.price = { $gte: minPrice };
+                }
+                if ((/-\d+/gm).test(price) && (/(\d+)-(\d+)/gm).test(price)) {
+                    let maxPrice = priceGroups[1];
+                    filter.price = { $lte: maxPrice };
+                } else if ((/-\d+/gm).test(price)) { // If we're only filtering by least than
+                    let maxPrice = priceGroups[0];
+                    filter.price = { $lte: maxPrice };
+                }
+            } else if ((/\d+/gm).test(price)) { // otherwise, it will be set just like given, and we will only get the exact match/es.
+                filter.price = price;
+            }
         }
 
         if (sale) {
@@ -59,7 +70,10 @@ router.get('/', async function (req, res, next) {
         }
 
         const ads = await Ads.list(filter, limit, skip, sort, fields);
-        res.json(ads);
+        // res.json(ads);
+        console.log(ads[0].name);
+
+        res.render('ads.ejs', { title: 'KiaraPop', ads: ads });
     } catch (err) {
         next(err);
     }
@@ -92,6 +106,7 @@ router.post('/', async (req, res, next) => {
         // finally save it on database
         const adSaved = await ad.save();
 
+        console.log(adData)
         // respond with the created ad
         res.json({ result: adSaved });
 
